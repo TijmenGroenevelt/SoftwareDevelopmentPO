cd07eaa287629264620228d748dc713aa781764b
TijmenGroenevelt <111001@st-maartenscollege.nl>
2023-02-20 14:04:00 +0100

    tickets pagina toegevoegd

--- SoftwareDevelopmentPO/Controllers/HomeController.cs
+++ SoftwareDevelopmentPO/Controllers/HomeController.cs
@@ -25,6 +25,12 @@ namespace SoftwareDevelopmentPO.Controllers
             return View();
         }
 
+        [Route("tickets")]
+        public IActionResult Tickets()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
